6a616f2fc42db860b952478b43a02e50b1fe58c6
josue valencia <josue.valrob@gmail.com>
2020-02-21 20:18:53 +0100

    styyled components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Title, Wrapper} from './styles/General'
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>
+        Hello World!
+      </Title>
+    </Wrapper>
+  );
+}
+
+export default App;
